1aeb791018b8d7f40fe87b724a4cbc09ae2b580b
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-09-12 14:36:44 +0200

    ADD : fonctionnel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  pseudo: string = '';
+  niveaux: string[] = ['facile', 'normal', 'difficile', 'extreme'];
+  form_isHidden: boolean = false;
+  message_isHidden: boolean = true;
+  question_isHidden: boolean = true;
+  suivant_isHidden: boolean = true;
+  error_length: string = 'Votre mot de passe doit faire plus de 3 caractères';
+  reponses: string[] = ['A', 'B', 'C', 'D'];
   constructor() {}
 
+  button_commencer_click() {
+    if (this.pseudo.length < 3) {
+      this.message_isHidden = false;
+    }else{
+      this.form_isHidden = true;
+      this.question_isHidden = false;
+    }
+  }
+
+  button_reponse_click(){
+    this.suivant_isHidden = false;
+  }
 }
